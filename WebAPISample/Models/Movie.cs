11995f2d51095bafc9f54d8a3638f34b804b1114
Bill <william.hine2019@gmail.com>
2020-06-30 13:18:58 -0400

    Added Year And ImageUrl Properties To Movie Model

--- WebAPISample/Models/Movie.cs
+++ WebAPISample/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,16 @@ namespace WebAPISample.Models
     {
         // We dont specify [Key] here becuase by using conventions it knows MovieId is the PK based off of its name.
         public int MovieId { get; set; }
+
+        [Required(ErrorMessage ="Movie Title Required")]
         public string Title { get; set; }
+
         public string Director { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Year { get; set; }
+
+        public string ImageURL { get; set; }
     }
 }
