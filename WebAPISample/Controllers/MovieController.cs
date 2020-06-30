5f1789b9bce4bd9d8842628ca3ea9477e0e894aa
Bill <william.hine2019@gmail.com>
2020-06-30 14:31:02 -0400

    We pushed as a master copy becuase we worked each function together, Complete all api methods

--- WebAPISample/Controllers/MovieController.cs
+++ WebAPISample/Controllers/MovieController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.View;
 using WebAPISample.Data;
 using WebAPISample.Models;
 
@@ -23,7 +24,9 @@ namespace WebAPISample.Controllers
         public IActionResult Get()
         {
             // Retrieve all movies from db logic
-            return Ok(new string[] { "movie1 string", "movie2 string" });
+            var movies = _context.Movies.ToList();
+          
+            return Ok(movies);
         }
 
         // GET api/movie/5
@@ -31,32 +34,63 @@ namespace WebAPISample.Controllers
         public IActionResult Get(int id)
         {
             // Retrieve movie by id from db logic
+            var returnedMovie = _context.Movies.Where(m => m.MovieId == id).SingleOrDefault();
             // return Ok(movie);
-            return Ok();
+            return Ok(returnedMovie);
         }
 
         // POST api/movie
         [HttpPost]
-        public IActionResult Post([FromBody]Movie value)
+        public IActionResult Post([FromBody]Movie movie)
         {
-            // Create movie in db logic
-            return Ok();
+            try
+            {
+                // Create movie in db logic
+                _context.Movies.Add(movie);
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch
+            {
+                return NotFound();
+            }
         }
-
         // PUT api/movie
         [HttpPut]
         public IActionResult Put([FromBody] Movie movie)
         {
-            // Update movie in db logic
-            return Ok();
+            try
+            {
+                // Update movie in db logic
+                var movieFromDB = _context.Movies.Where(m => m.MovieId == movie.MovieId).SingleOrDefault();
+                movieFromDB.Title = movie.Title;
+                movieFromDB.Director = movie.Director;
+                movieFromDB.Genre = movie.Genre;
+                movieFromDB.Year = movie.Year;
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch
+            {
+                return NotFound();
+            }
         }
 
         // DELETE api/movie/5
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            // Delete movie from db logic
-            return Ok();
+            try
+            {
+                var movieToDelete = _context.Movies.Find(id);
+                _context.Movies.Remove(movieToDelete);
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch
+            {
+                return NotFound();
+            }
         }
     }
 }
