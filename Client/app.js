e40b1a4e529ba2e716f2f29b02209bcb46a6cb87
Bill <william.hine2019@gmail.com>
2020-07-01 15:19:19 -0400

    Added code to make movies display in table row

--- Client/app.js
+++ Client/app.js
@@ -4,9 +4,16 @@ $(function(){
         console.log(data);
 
         for(let i = 0; i < data.length; i++){
-            $("#MovieList").append(
-                `<div>${JSON.stringify(data[i].title).substring(1, data[i].title.length + 1)} ${JSON.stringify(data[i].year).substring(1, data[i].year.length + 1)}</div>`
+           
+           $("#movieTitle").append(`<div class="titleheader">${JSON.stringify(data[i].title).substring(1, data[i].title.length + 1)}</div>`
+                
             )
-        }
+            $("#movieYear").append(`<div class="titleheader">${JSON.stringify(data[i].year).substring(1, data[i].year.length + 1)}</div>`
+                
+            )
+            
+        } 
     })
+    
+
 })
