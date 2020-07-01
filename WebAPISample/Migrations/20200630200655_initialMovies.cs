a23a575c44215792e9a6681c7ad6d093af95f2a5
unknown <mstiff1983@gmail.com>
2020-07-01 12:34:21 -0400

    Added The Notebook to database

---
+++ WebAPISample/Migrations/20200630200655_initialMovies.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPISample.Migrations
+{
+    public partial class initialMovies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movies",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Genre",
+                table: "Movies",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "Movies",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Year",
+                table: "Movies",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "MovieId", "Director", "Genre", "ImageURL", "Title", "Year" },
+                values: new object[,]
+                {
+                    { 1, "Martin Scorsese", "Drama", null, "The Departed", "2006" },
+                    { 2, "Christopher Nolan", "Drama", null, "The Dark Knight", "2008" },
+                    { 3, "Christopher Nolan", "Drama", null, "Inception", "2010" },
+                    { 4, "David Gordon Green", "Comedy", null, "Pineapple Express", "2008" },
+                    { 5, "John McTiernan", "Action", null, "Die Hard", "1988" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 5);
+
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "Movies");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
