5c48eb1a28f91d66fd6ce10e3f4c3e4bf17a9cda
unknown <mstiff1983@gmail.com>
2020-07-01 13:38:39 -0400

    There was a function added the app.js file to upload a list of Movies listed by Name and Year

--- Client/app.js
+++ Client/app.js
@@ -1,34 +1,12 @@
-/*
-(function($){
-    function processForm( e ){
-        var dict = {
-        	Title : this["title"].value,
-        	Director: this["director"].value
-        };
-
-        $.ajax({
-            url: 'https://localhost:44325/api/movie',
-            dataType: 'json',
-            type: 'post',
-            contentType: 'application/json',
-            data: JSON.stringify(dict),
-            success: function( data, textStatus, jQxhr ){
-                $('#response pre').html( data );
-            },
-            error: function( jqXhr, textStatus, errorThrown ){
-                console.log( errorThrown );
-            }
-        });
-
-        e.preventDefault();
-    }
-
-    $('#my-form').submit( processForm );
-})(jQuery);
-*/
 
 $(function(){
     $.get("https://localhost:44325/api/movie/", function(data){
         console.log(data);
+
+        for(let i = 0; i < data.length; i++){
+            $("#MovieList").append(
+                `<div>${JSON.stringify(data[i].title).substring(1, data[i].title.length + 1)} ${JSON.stringify(data[i].year).substring(1, data[i].year.length + 1)}</div>`
+            )
+        }
     })
 })
