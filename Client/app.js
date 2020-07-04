efde06296250abeca1c8fc44f03d92e0d5b1e262
Andrew K <drummer4life93@yahoo.com>
2020-07-03 22:05:57 -0500

    Added function to bring up modal with a populated form when clicking on respective edit button on the table.

--- Client/app.js
+++ Client/app.js
@@ -1,32 +1,84 @@
-
-$(function(){
+"use strict";
+$(document).ready(function() {
+    
     $.get("https://localhost:44325/api/movie/", function(data){
         console.log(data);
-
+        
         for(let i = 0; i < data.length; i++)
         {
-           //if(i == 0){
-           $("#movieList1").append(`<tr><td class="titleheader">${JSON.stringify(data[i].title).substring(1, data[i].title.length + 1)}</td>
-           <td class="titleheader">${JSON.stringify(data[i].year).substring(1, data[i].year.length + 1)}</td><td><button type="button" class="btn btn-primary style-btn">Details</button><button type="button" class="btn btn-primary style-btn">Edit</button></td> </tr>`
-                
+            $("#movieList1").append(`<tr><td class="titleheader">${JSON.stringify(data[i].title).substring(1, data[i].title.length + 1)}</td>
+           <td class="titleheader">${JSON.stringify(data[i].year).substring(1, data[i].year.length + 1)}
+           </td><td><button type="button" class="btn btn-primary style-btn">Details</button>
+           <button id="editButton" type="button" class="btn btn-primary style-btn" 
+            data-toggle="modal" data-target="#editModal"  onclick="populate(`+i+`)")>Edit</button></td></tr>`
+            
+            
             )
-        //     $("#movieYear").append(``
-                
-        //     )
-        //     console.log("In if statement");
-        //    }
-        //    else
-        //    {
-        //         //The HTML of the TR row that we want to add to our table.
-        //         var title = $("#movieTitle").add(`<div class="titleheader">${JSON.stringify(data[i].title).substring(1, data[i].title.length + 1)}</div>`);
-        //         var newTableRow = '<tr data-toggle="collapse" data-target="#accordion" class="clickable collapse-row collapsed"><td id="movieTitle">' + title + '</td></tr> <tr><td colspan="3"><div id="accordion" class="collapse">Movie Details <br></br> <button type="button" class="btn btn-primary">Edit</button></div></td></tr>';
-
-        //         //Add the HTML after the last row by using tr:last.
-        //         $('#my_table tr:last').after(newTableRow); 
-
-        //    }
         } 
+        
+        
     })
     
+    
+})
+
+function populate(i)
+{
+    let id = i+1;
+    $.get("https://localhost:44325/api/movie/", function(data){
+
+    $(".modal-body").html(`<form id="updateForm">
+    <input type="hidden" id="movieId" name="movieId" value=`+id+`>
+    <div class="form-group">
+      <label for="titleform">Title:</label>
+      <input type="title" class="form-control" placeholder="` + data[i].title + `" id="titleForm">
+    </div>
+    <div class="form-group">
+      <label for="directform">Director:</label>
+      <input type="director" class="form-control" placeholder="` + data[i].director + `" id="directorForm">
+    </div>
+    <div class="form-group">
+        <label for="genreform">Genre:</label>
+        <input type="genre" class="form-control" placeholder="` + data[i].genre + `" id="genreForm">
+      </div>
+      <div class="form-group">
+        <label for="yearform">Year:</label>
+        <input type="year" class="form-control" placeholder="` + data[i].year + `" id="yearForm">
+      </div>
+    <button id="updateMovie" type="submit" class="btn btn-primary">Submit</button>
+  </form>`)
+
 
 })
+}
+
+
+
+
+$("#updateMovie").submit( function(){
+    var updatedMovie = {
+        movieId: this["movieId"].value(),
+        title: this["titleForm"].value(),
+        director: this["directorForm"].value(),
+        genre: this["genreForm"].value(),
+        year: this["yearForm"].value()
+    }
+    console.log(updatedMovie.movieId)
+    putMovie(updatedMovie.movieId, updatedMovie);
+  
+})
+
+function putMovie(id, data){
+$.ajax({
+    url: "https://localhost:44325/api/movie/" + id,
+    type: 'put',
+    dataType: 'json',
+    contentType: 'application/json',
+    data: data,
+    success: function( data ) {
+      
+        
+    }
+  });
+}
+
