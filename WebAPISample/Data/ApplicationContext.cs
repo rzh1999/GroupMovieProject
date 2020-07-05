77fbbca382dd30725f3bf692591921cd26de3565
Bill <william.hine2019@gmail.com>
2020-07-05 19:58:36 -0400

    Changed seed to include image urls

--- WebAPISample/Data/ApplicationContext.cs
+++ WebAPISample/Data/ApplicationContext.cs
@@ -19,11 +19,11 @@ namespace WebAPISample.Data
             // Seed data - needs migration
             modelBuilder.Entity<Movie>()
              .HasData(
-                new Movie { MovieId = 1, Title = "The Departed", Genre = "Drama", Director = "Martin Scorsese", Year = "2006" },
-                new Movie { MovieId = 2, Title = "The Dark Knight", Genre = "Drama", Director = "Christopher Nolan", Year = "2008" },
-                new Movie { MovieId = 3, Title = "Inception", Genre = "Drama", Director = "Christopher Nolan", Year = "2010" },
-                new Movie { MovieId = 4, Title = "Pineapple Express", Genre = "Comedy", Director = "David Gordon Green", Year = "2008" },
-                new Movie { MovieId = 5, Title = "Die Hard", Genre = "Action", Director = "John McTiernan", Year = "1988" }
+                new Movie { MovieId = 1, Title = "The Departed", Genre = "Drama", Director = "Martin Scorsese", Year = "2006" , ImageURL ="./images/TheDeparted.jpg" },
+                new Movie { MovieId = 2, Title = "The Dark Knight", Genre = "Drama", Director = "Christopher Nolan", Year = "2008", ImageURL = "./images/TheDarkKnight.jpg" },
+                new Movie { MovieId = 3, Title = "Inception", Genre = "Drama", Director = "Christopher Nolan", Year = "2010", ImageURL = "./images/Inception.jpg"},
+                new Movie { MovieId = 4, Title = "Pineapple Express", Genre = "Comedy", Director = "David Gordon Green", Year = "2008", ImageURL = "./images/PineappleExpress.jpg" },
+                new Movie { MovieId = 5, Title = "Die Hard", Genre = "Action", Director = "John McTiernan", Year = "1988", ImageURL = "./images/DieHard.jpg" }
              );
 
         }
